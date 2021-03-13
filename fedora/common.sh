f4a7f8a8288e6c55a07e6ca455cedb21fc18a2f5
Roy Portas <royportas@gmail.com>
2021-03-13 11:09:47 +1000

    Added fedora scripts

---
+++ fedora/common.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Installs common tools for a base Fedora system,
+# this includes extra repos like RPM Fusion
+#
+
+# Exit when any command fails
+set -e
+# Treat unset variables as an error
+set -u
+# Print all commands to stdout
+set -x
+
+# Install RPM Fusion
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Update system packages
+sudo dnf update -y
+
+# Install common packages
+sudo dnf install -y \
+  vim \
+  htop \
+  vlc \
+  VirtualBox \
+  vagrant \
+  dnf-plugins-code # Used by docker script
---
+++ fedora/docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Installs common tools for a base Fedora system,
+# this includes extra repos like RPM Fusion
+#
+
+# Exit when any command fails
+set -e
+# Treat unset variables as an error
+set -u
+# Print all commands to stdout
+set -x
+
+# Setup the stable repository
+sudo dnf config-manager \
+  --add-repo \
+  https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+# Create the docker group and add the current user
+sudo groupadd docker
+sudo usermod -aG docker $USER
