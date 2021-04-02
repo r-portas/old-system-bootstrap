e48cfc4d01cd8fe075d684f07049b052ab9c968b
Roy Portas <royportas@gmail.com>
2021-04-02 14:04:48 +1000

    Work on fedora_workstation

--- fedora_workstation/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exit script on error
-set -e
-
-sudo dnf install -y ansible
-ansible-playbook playbook.yml --ask-become-pass
--- fedora_workstation/install.sh
+++ fedora_workstation/install.sh
@@ -8,9 +8,9 @@
 set -e
 
 # Install git, to clone the repo
-sudo dnf install -y git
+sudo dnf install -y git make
 
 # Clone the repo
 cd ~ && git clone https://github.com/r-portas/system-bootstrap.git
 
-cd ~/system-bootstrap/fedora_server/ && bash bootstrap.sh
+cd ~/system-bootstrap/fedora_server/ && make bootstrap
